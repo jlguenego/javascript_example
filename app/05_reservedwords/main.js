0a5224aff2ca04a71c7cdc7ab183ca7a229bb548
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-07-02 14:15:22 +0200

    add: try catch finally

--- app/05_reservedwords/main.js
+++ app/05_reservedwords/main.js
@@ -3,13 +3,43 @@
 
 	// Label and break
 	coucou: {
-		console.log('coucou');
-		{
+		console.log('coucou'); {
 			console.log('hello');
 			break coucou;
 		}
 		// eslint-disable-next-line no-unreachable
 		console.log('never reached.');
+	}
+
+	// switch and case.
+	var myValue = 'coucou';
+	switch (myValue) {
+		case 1:
+			console.log('This is 1.');
+			break;
+		case 2:
+			console.log('This is 2.');
+			break;
+		case 'coucou':
+			console.log('This is coucou.');
+			break;
+		default:
+			console.log('This is default.');
+	}
+
+
+	// try catch finally
+	try {
+		try {
+			throw new Error('oops');
+		} catch (ex) {
+			console.error('inner', ex.message);
+			throw ex;
+		} finally {
+			console.log('finally');
+		}
+	} catch (ex) {
+		console.error('outer', ex.message);
 	}
 
 
